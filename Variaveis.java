2837e264be23a4171b745c2a1843f7bbea14bc38
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2022-03-05 15:29:03 -0300

    teste

---
+++ Variaveis.java
@@ -0,0 +1,42 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        // Tipo ~ nome ~ value
+            //variaveis do tipo inteiro:
+                //menor numero = -128  maior numero = 127
+                byte numeroPequeno = 127;
+                System.out.println(numeroPequeno);
+
+                //menor numero = -32768 maior numero = 32767
+                short numeroShort = 32767;
+                System.out.println(numeroShort);
+
+                // maior numero = -2147483648 menor numero = 2147483647
+                int numeroInteiro = 2147483647;
+                System.out.println(numeroInteiro);
+
+                //menor numero =  maior numero =
+                long numerolong = 922337203685477000l;
+                System.out.println(numerolong);
+            
+            //variaveis do tipo flutuante
+                float numeroFlutuando = 2.5f;
+                System.out.println(numeroFlutuando);
+
+                double numeroflutuante2 = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620898628034825342117067;
+                System.out.println(numeroflutuante2);
+        
+            //variaveis do tipo caractere
+                char letra = 'c';
+                System.out.println(letra);
+            
+            //variaveis do tipo boolean
+
+                boolean boleana = false; // true ou false
+                System.out.println(boleana);
+
+            //variaveis do tipo string TIPO CLASSE e objeto
+
+                String nome = "Leonardo";
+                System.out.println(nome);
+    }
+}
