8d80032c00b21c1458a39dea968a49e550584695
Leonardo Pinheiro <94575742+SrPinheiro@users.noreply.github.com>
2022-04-13 19:11:40 -0300

    Create try-catch.java

---
+++ BASE-JAVA/try-catch.java
@@ -0,0 +1,27 @@
+public class try-catch{
+    public static void main(String[] args){
+     /* try{
+          #(codigo)
+          
+          #o Try vai tentar executar um codigo que você criou, caso aconteça algum erro, o try sera encerrado e o catch continuara o codigo
+          
+         }
+         catch(Exception e){
+              #(codigo)
+              
+              #quando acontecer 
+          }
+          catch(Exception2 e){
+              #(codigo)
+              
+          }
+          
+          finally{
+            #codigo
+          }
+         
+          
+     */     
+          
+    }
+}
