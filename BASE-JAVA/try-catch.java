57166c74a65bfe296caec78ea3f9c3323333f620
Leonardo Pinheiro <94575742+SrPinheiro@users.noreply.github.com>
2022-04-13 23:14:52 -0300

    Update try-catch.java

--- BASE-JAVA/try-catch.java
+++ BASE-JAVA/try-catch.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class try-catch{
     public static void main(String[] args){
      /* try{
@@ -9,19 +12,39 @@ public class try-catch{
          catch(Exception e){
               #(codigo)
               
-              #quando acontecer 
+              #quando acontecer um erro ele ira executar o catch! "Exception E" é um erro universal, se colocar esse parametro qualquer erro que acontecer
+              #ele sera jogado no catch com esse parametro
           }
           catch(Exception2 e){
               #(codigo)
+              
+              # vc tbm pode colocar Exception  exclusivos como o "InputMisMatchException" que é um erro de entrada invalida, colocando esse parametro o catch so sera
+              # executado caso aconteça este tipo de erro!
               
           }
           
           finally{
             #codigo
+            
+            #tudo que colocar no finally sera executado, independente se o codigo deu erro ou não!!
           }
          
-          
+       exemplo:
      */     
           
+        try{
+            Scanner scan = new Scanner(System.in);
+        }
+        catch(InputMismatchException e){
+            System.out.printf("isso aqui so acontecera se ouver erros de entrada!");
+        }
+        
+        catch(Exception e){
+            System.out.println("isso aqui aparecera para qualquer erro!");
+        }
+        
+        finally{
+            System.out.println("isso aqui sempre vai aparecer, independente se deu erro ou não");
+        }
     }
 }
