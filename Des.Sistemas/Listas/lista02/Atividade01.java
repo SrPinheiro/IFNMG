2ddb918bc525d74d89239cadd3d3b3d911e1627a
Leonardo Pinheiro <Sr.Pinheiro.IFNMG@gmail.com>
2022-06-10 08:40:43 -0300

    Lista02

---
+++ Des.Sistemas/Listas/lista02/Atividade01.java
@@ -0,0 +1,12 @@
+
+package lista02;
+
+public class Atividade01 {
+    public static void main(String[] args) {
+        for(byte i = 1; i <= 50; i++){
+            System.out.printf("Numero: %d\n", i);
+        }
+        
+    }
+    
+}
