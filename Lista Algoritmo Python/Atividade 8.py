736873f26bc82c3b48e181e74a2bd962357d5beb
Leonardo Pinheiro <Sr.Pinheiro.IFNMG@gmail.com>
2022-06-26 04:18:18 -0300

    Delete Atividade 8.py

--- Lista Algoritmo Python/Atividade 8.py
+++
@@ -1,8 +0,0 @@
-"""8. Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10."""
-var1=float(input('Digite um numero:'))
-if var1 >10:
-    print(f'O número {var1} é maior que 10!')
-elif var1 == 10:
-    print(f'o numero que você digitou é igual a 10!')
-else:
-    print(f'O numero {var1} é menor que 10!')
