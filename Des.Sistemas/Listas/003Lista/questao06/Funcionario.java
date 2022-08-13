a9fb31064f0651f9b7e98fb63a6481b91a5ca927
Leonardo Pinheiro <Sr.Pinheiro.IFNMG@gmail.com>
2022-08-13 20:52:55 -0300

    questao06

--- Des.Sistemas/Listas/003Lista/questao06/Funcionario.java
+++ Des.Sistemas/Listas/003Lista/questao06/Funcionario.java
@@ -1,9 +1,54 @@
 package questao06;
 
+import java.util.Scanner;
+
 public class Funcionario {
     private String nome;
     private String cpf;
-    private int quantdadeDeFilhos;
+    private int quantidadeDeFilhos;
     private double salarioBase;
 
+    Funcionario(String nome, String cpf, int quantidadeDeFilhos, double salarioBase){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.quantidadeDeFilhos = quantidadeDeFilhos;
+        this.salarioBase = salarioBase;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getQuantidadeDeFilhos() {
+        return quantidadeDeFilhos;
+    }
+
+    public void setQuantidadeDeFilhos(int quantidadeDeFilhos) {
+        this.quantidadeDeFilhos = quantidadeDeFilhos;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalario(){
+        return this.salarioBase + (this.quantidadeDeFilhos * 100);
+    }
 }
